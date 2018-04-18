c086efbe6bb5c3bc711907e2babf594b7d882c66
hzz113-admin <hzz113-admin@HZHONG-MACBOOK.local>
2018-04-18 18:56:09 -0400

    first attempt

---
+++ main.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import numpy as np
+from copy import deepcopy
+from tensorflow.examples.tutorials.mnist import input_data
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from IPython import display
+
+from model import Model
+
+def mnist_imshow(img):
+    plt.imshow(img.reshape([28,28]), cmap="gray")
+    plt.axis('off')
+
+# return a new mnist dataset w/ pixels randomly permuted
+def permute_mnist(mnist):
+    perm_inds = range(mnist.train.images.shape[1])
+    np.random.shuffle(perm_inds)
+    mnist2 = deepcopy(mnist)
+    sets = ["train", "validation", "test"]
+    for set_name in sets:
+        this_set = getattr(mnist2, set_name) # shallow copy
+        this_set._images = np.transpose(np.array([this_set.images[:,c] for c in perm_inds]))
+    return mnist2
+
+def plot_test_acc(plot_handles):
+    plt.legend(handles=plot_handles, loc="center right")
+    plt.xlabel("Iterations")
+    plt.ylabel("Test Accuracy")
+    plt.ylim(0,1)
+    display.display(plt.gcf())
+    display.clear_output(wait=True)
+    
+# train/compare vanilla sgd and ewc
+def train_task(model, num_iter, disp_freq, trainset, testsets, x, y_, lams=[0]):
+    for l in range(len(lams)):
+        # lams[l] sets weight on old task(s)
+        model.restore(sess) # reassign optimal weights from previous training session
+        if(lams[l] == 0):
+            model.set_vanilla_loss()
+        else:
+            model.update_ewc_loss(lams[l])
+        # initialize test accuracy array for each task 
+        test_accs = []
+        for task in range(len(testsets)):
+            test_accs.append(np.zeros(num_iter/disp_freq))
+        # train on current task
+        for iter in range(num_iter):
+            batch = trainset.train.next_batch(100)
+            model.train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+            if iter % disp_freq == 0:
+                plt.subplot(1, len(lams), l+1)
+                plots = []
+                colors = ['r', 'b', 'g']
+                for task in range(len(testsets)):
+                    feed_dict={x: testsets[task].test.images, y_: testsets[task].test.labels}
+                    test_accs[task][iter/disp_freq] = model.accuracy.eval(feed_dict=feed_dict)
+                    c = chr(ord('A') + task)
+                    plot_h, = plt.plot(range(1,iter+2,disp_freq), test_accs[task][:iter/disp_freq+1], colors[task], label="task " + c)
+                    plots.append(plot_h)
+                plot_test_acc(plots)
+                if l == 0: 
+                    plt.title("vanilla sgd")
+                else:
+                    plt.title("ewc")
+                plt.gcf().set_size_inches(len(lams)*5, 3.5)
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.InteractiveSession()
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+model = Model(x, y_)
+sess.run(tf.global_variables_initializer())
+train_task(model, 800, 20, mnist, [mnist], x, y_, lams=[0])
+
+model.compute_fisher(mnist.validation.images, sess, num_samples=200, plot_diffs=True) # use valida
+mnist2 = permute_mnist(mnist)
+model.star()
+train_task(model, 800, 20, mnist2, [mnist, mnist2], x, y_, lams=[0, 15])
+
+model.compute_fisher(mnist2.validation.images, sess, num_samples=200, plot_diffs=True)
+mnist3 = permute_mnist(mnist)
+model.star()
+
+train_task(model, 800, 20, mnist3, [mnist, mnist2, mnist3], x, y_, lams=[0, 15])
